81d8a3fa10621137cce0ff6000a017d7ecb79126
HUZAIFA SHAHAB <huzaifarifatullah@gmail.com>
2023-07-30 10:11:05 +0500

    Update spiralMatrixCode.java

--- spiralMatrixCode.java
+++ spiralMatrixCode.java
@@ -1,3 +1,18 @@
 public class spiralMatrixCode {
-    
+    public static void main(String[] args) {
+        int [][] multiArray ={{1,2,3,4},
+                              {5,6,7,8},
+                            {9,10,11,12},
+                            {13,14,15,16}} ;
+
+        int startRow = 0 ;
+        int startCol = 0 ;
+        int endRow = multiArray.length-1 ;
+        int endCol = multiArray[0].length-1 ;
+
+        while (startRow <= endRow && startCol <= endCol){
+            //Top
+            
+        }
+    }
 }
